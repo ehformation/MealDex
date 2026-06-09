a837987708aecd69534a134a26668d0eabb6d9ae
codewithelie <me@elyh.fr>
2026-06-09 12:14:47 +0200

    Mise à jour de la configuration de routage pour intégrer MealListComponent et remplacement du composant MealList par un router-outlet dans app.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MealListComponent } from './meals/meal-list/meal-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MealListComponent }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
